acab58d77038ecb57619632a1b54345ef33b9946
Durbek <saydaliyevdurbek0512@gmail.com>
2024-12-31 15:12:41 +0500

    cleares unused imports

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,5 +1,5 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
